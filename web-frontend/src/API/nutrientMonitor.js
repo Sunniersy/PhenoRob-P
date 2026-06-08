3db8535f7a935abaf15306b26f9b665594acb756
SHAOYANG <109461982+Sunniersy@users.noreply.github.com>
2026-06-08 11:56:38 +0800

    Delete web-frontend directory

--- web-frontend/src/API/nutrientMonitor.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-// 养分监测
-export function getNtrData(){ 
-    return request({
-      url: '/greenhouse/data/all',
-      method: 'get', 
-      timeout: 1000*600
-    })
-  }
